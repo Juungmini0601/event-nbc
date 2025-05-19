a4138c2634718855264e9e5552aad2c5e6651dab
hugecookie <phrpp5770@gmail.com>
2025-05-19 14:15:03 +0900

    feat: Event 엔티티에 확률 기반 선물 획득 로직 구현

--- src/main/java/event/nbc/model/Event.java
+++ src/main/java/event/nbc/model/Event.java
@@ -1,14 +1,48 @@
 package event.nbc.model;
 
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Event {
-	private Long eventId;
+
+	@Getter
+    private Long eventId;
 	private AtomicLong remainingCount;
 	private List<String> imageUrls; // ConcurrentLinkedQueue, LinkedBlockingQueue, ArrayBlockingQueue, PriorityBlockingQueue, SynchronousQueue
 
-	private LocalDateTime startAt;
-	private LocalDateTime endAt;
+	@Getter
+    private LocalDateTime startAt;
+	@Getter
+    private LocalDateTime endAt;
+
+	public Event(Long eventId, int remainingCount, List<String> imageUrls,
+				 LocalDateTime startAt, LocalDateTime endAt) {
+		this.eventId = eventId;
+		this.remainingCount = new AtomicLong(remainingCount);
+		this.imageUrls = imageUrls;
+		this.startAt = startAt;
+		this.endAt = endAt;
+	}
+
+    public long getRemainingCount() {
+		return remainingCount.get();
+	}
+
+	public synchronized String tryClaimImageUrlWithChance() {
+		if (Math.random() > 0.3) {
+			return null;
+		}
+
+		long currentCount = remainingCount.get();
+		if (currentCount <= 0) return null;
+
+		long imageIndex = imageUrls.size() - currentCount;
+		if (imageIndex < 0 || imageIndex >= imageUrls.size()) return null;
+
+		remainingCount.decrementAndGet();
+		return imageUrls.get((int) imageIndex);
+	}
 }
