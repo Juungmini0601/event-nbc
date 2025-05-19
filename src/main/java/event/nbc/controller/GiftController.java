2eb4a55c62c3f7a7efb15200b903b628b2ac22b5
hugecookie <phrpp5770@gmail.com>
2025-05-19 14:17:33 +0900

    feat: 이벤트 최초 진입 시 저장소에 이벤트 등록 처리

---
+++ src/main/java/event/nbc/controller/GiftController.java
@@ -0,0 +1,50 @@
+package event.nbc.controller;
+
+import event.nbc.model.Event;
+import event.nbc.repository.EventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class GiftController {
+
+    private final EventRepository eventRepository;
+
+    @GetMapping("/event/{eventId}")
+    public String enterEventPage(@PathVariable Long eventId, Model model) {
+        Event event = eventRepository.findById(eventId);
+        if (event == null) {
+            event = new Event(
+                    eventId,
+                    3,
+                    List.of(
+                            "https://dummyimage.com/300x200/000/fff&text=기프티콘1",
+                            "https://dummyimage.com/300x200/111/eee&text=기프티콘2",
+                            "https://dummyimage.com/300x200/222/eee&text=기프티콘3"
+                    ),
+                    LocalDateTime.now().plusMinutes(1),
+                    LocalDateTime.now().plusMinutes(10)
+            );
+            eventRepository.save(event);
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        boolean isBeforeStart = now.isBefore(event.getStartAt());
+        boolean isEnded = now.isAfter(event.getEndAt());
+        boolean isAvailable = event.getRemainingCount() > 0 && !isBeforeStart && !isEnded;
+
+        model.addAttribute("event", event);
+        model.addAttribute("isBeforeStart", isBeforeStart);
+        model.addAttribute("isEnded", isEnded);
+        model.addAttribute("isAvailable", isAvailable);
+
+        return "gift-page";
+    }
+}
