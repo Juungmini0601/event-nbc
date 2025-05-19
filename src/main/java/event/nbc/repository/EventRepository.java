f4591fe8c1ac56e3a382b449a8f7ead2769956c6
hugecookie <phrpp5770@gmail.com>
2025-05-19 14:17:09 +0900

    chore: 이벤트 저장소 클래스 기본 생성

---
+++ src/main/java/event/nbc/repository/EventRepository.java
@@ -0,0 +1,20 @@
+package event.nbc.repository;
+
+import event.nbc.model.Event;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class EventRepository {
+    private final Map<Long, Event> eventMap = new ConcurrentHashMap<>();
+
+    public void save(Event event) {
+        eventMap.put(event.getEventId(), event);
+    }
+
+    public Event findById(Long id) {
+        return eventMap.get(id);
+    }
+}
