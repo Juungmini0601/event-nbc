9c7be068d15cf9b9639c408caf37dcb9f7ea161a
hugecookie <phrpp5770@gmail.com>
2025-05-21 21:47:33 +0900

    feat: eventRedisRepository로 불러오게 변경

--- src/main/java/event/nbc/event/controller/GiftController.java
+++ src/main/java/event/nbc/event/controller/GiftController.java
@@ -1,7 +1,7 @@
 package event.nbc.event.controller;
 
+import event.nbc.event.repository.EventRedisRepository;
 import event.nbc.model.Event;
-import event.nbc.event.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,32 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class GiftController {
 
-    private final EventRepository eventRepository;
+    private final EventRedisRepository eventRedisRepository;
 
     @GetMapping("/event/{eventId}")
     public String enterEventPage(@PathVariable Long eventId, Model model) {
-        Event event = eventRepository.findById(eventId);
-        // 나중에 삭제해야함
-        if (event == null) {
-            event = new Event(
-                    eventId,
-                    3,
-                    List.of(
-                            "https://dummyimage.com/300x200/000/fff&text=기프티콘1",
-                            "https://dummyimage.com/300x200/111/eee&text=기프티콘2",
-                            "https://dummyimage.com/300x200/222/eee&text=기프티콘3"
-                    ),
-                    LocalDateTime.now().minusMinutes(1),
-                    LocalDateTime.now().plusMinutes(10)
-            );
-            eventRepository.save(event);
-        }
+        Event event = eventRedisRepository.findById(eventId);
 
         LocalDateTime now = LocalDateTime.now();
         boolean isBeforeStart = now.isBefore(event.getStartAt());
